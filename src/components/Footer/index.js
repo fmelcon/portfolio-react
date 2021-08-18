e9970cbc14840d4e485ffb1f18f307226e2a84a4
Franco Melcon <fmelcon@live.com>
2021-08-18 19:23:01 -0300

    adding footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,29 +6,30 @@ const Footer = () => {
     <footer className="footer">
       <div className="footer-info">
         <h1>Franco Melcon</h1>
+        <p>Fron End Developer</p>
       </div>
       <div className="footer-contact">
-        <h3>Contact Me</h3>
+        <h3>Contact me</h3>
+        <p>And let's get work together</p>
       </div>
       <div className="footer-sns">
-        <div className="Design">Design By Franco Melcon</div>
         <div className="sns-links">
           <a
             target="_blank"
             rel="noopener noreferrer"
             href="https://www.linkedin.com/in/fmelcon/"
           >
-            <i className="fab fa-linkedin likedin"></i>
+            <i className="fab fa-linkedin linkedin"></i>
           </a>
           <a
             target="_blank"
             rel="noopener noreferrer"
             href="https://github.com/fmelcon"
           >
-            <i className="fab fa-github"></i>
+            <i className="fab fa-github github"></i>
           </a>
           <a target="_blank" rel="noopener noreferrer" href="http://">
-            <i className="fab fa-facebook f"></i>
+            <i className="fab fa-facebook facebook"></i>
           </a>
         </div>
       </div>
